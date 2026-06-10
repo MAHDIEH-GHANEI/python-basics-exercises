97430b7533b6361fa43121bfb15c58320964c8bb
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-06-10 13:24:25 +0330

    Add files via upload

---
+++ practice_6.py
@@ -0,0 +1,5 @@
+# Write a program that asks for the user's name and age, and prints a welcome message.
+
+name = str(input("Please enter your name: "))
+age = int(input("Please enter your age: "))
+print(f"Hi {name}, your age is {age}") 
