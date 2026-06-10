7b1a8cfd64642750ea06c2dd6ceb55339a0a8ffe
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-06-10 13:26:37 +0330

    Add files via upload

---
+++ practice_7.py
@@ -0,0 +1,6 @@
+# Write a program that takes two numbers and prints their sum.
+
+number1=int(input("Please enter first number: "))
+number2=int(input("Please enter second number: "))
+sum_result = number1 + number2
+print(f"The sum is: {sum_result}")
