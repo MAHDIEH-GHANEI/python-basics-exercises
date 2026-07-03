76212ef7c80a7f1d23f05ce99f7a4669e4aceb56
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-03 22:42:05 +0330

    Delete practice_10.py

--- practice_10.py
+++
@@ -1,7 +0,0 @@
-# A program that calculates the length of a string
-# (your name) using the len() function
-
-
-name = str(input("Enter your name: "))
-lenght = len(name)
-print(f"The length of your name: {lenght}")
