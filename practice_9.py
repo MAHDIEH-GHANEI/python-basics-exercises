26dd85b398faee3959405c56a491b87bde88c31f
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-03 22:47:39 +0330

    Delete practice_9.py

--- practice_9.py
+++
@@ -1,7 +0,0 @@
-# Write a program that calculates the area of a rectangle.
-
-length = float(input("Enter length: "))
-width = float(input("Enter width: "))
-rectangle = length * width
-
-print(f"rectangle area is: {rectangle}")
