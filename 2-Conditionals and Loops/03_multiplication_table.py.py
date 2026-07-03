81c8b087ae38266203e6c715976d7d3ed758b364
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-03 22:05:41 +0330

    Delete 2-Conditionals and Loops directory

--- 2-Conditionals and Loops/03_multiplication_table.py.py
+++
@@ -1,5 +0,0 @@
-# A program that takes a number and prints the multiplication table of that number up to 10
-
-number = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(f"{number} * {i} = {number * i}")
