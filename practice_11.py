b09360e6da695b3fcd5a5f4a7f655e1dd4fc0b37
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-03 22:44:00 +0330

    Delete practice_11.py

--- practice_11.py
+++
@@ -1,8 +0,0 @@
-# Write a program that takes a number as input 
-# and determines whether it is even or odd (using the modulo operator `%`).
-
-number =int(input("Enter number: "))
-if int(number%2)==0:
-    print(number, "your number is even")
-else:
-    print(number, "your number is odd")
