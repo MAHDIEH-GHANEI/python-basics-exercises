57452ea1671757a120f77671cbd227814eb4f130
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-07 20:35:55 +0330

    Delete practice_32.py

--- practice_32.py
+++
@@ -1,9 +0,0 @@
-#“Write a program that counts the number of words in a sentence.”
-
-def count_words(sentence):
-    words = sentence.split()  #convert to list
-    return len(words) #return len(sentence.split())
-
-string = "I love python programming"
-
-print("Number of words in the sentence: ", count_words(string))
