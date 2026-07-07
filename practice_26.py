636e133828a1c7260c818918cd9aba66681c0e4d
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-07 20:33:08 +0330

    Delete practice_26.py

--- practice_26.py
+++
@@ -1,7 +0,0 @@
-#“Write a function that takes two numbers as arguments and returns their product.”
-
-def num(a , b):
-    sm = a * b
-    return sm
-result = num(4, 5)
-print("The product of two numbers: ", result)
