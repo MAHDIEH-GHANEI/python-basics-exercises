dcd059fcb0fa78b9d1cb3bd40677628c5ae2b694
Mahdie Ghanei <162366390+MAHDIEH-GHANEI@users.noreply.github.com>
2026-07-07 20:36:54 +0330

    Delete practice_30.py

--- practice_30.py
+++
@@ -1,7 +0,0 @@
-#“Write a program to convert units (for example, kilometers to meters).”
-
-def kilometer_to_meter(kilometer):
-    return kilometer * 1000
-
-result = kilometer_to_meter(float(input("Enter kilometer: ")))
-print("Kilometers to meters Entered number: ", result)
